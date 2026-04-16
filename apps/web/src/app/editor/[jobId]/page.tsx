65513b93847d28e97a22d1dc3bc55a086f1c8713
Bruce LIU <unknownseed@gmail.com>
2026-04-17 00:10:55 +0800

    Fix: add top padding to editor page to avoid overlapping with fixed navbar

--- apps/web/src/app/editor/[jobId]/page.tsx
+++ apps/web/src/app/editor/[jobId]/page.tsx
@@ -4,7 +4,7 @@ export default async function EditorPage({ params }: { params: Promise<{ jobId: 
   const { jobId } = await params;
   return (
     <div className="min-h-dvh">
-      <main className="mx-auto flex w-full max-w-6xl flex-col px-4 py-8">
+      <main className="mx-auto flex w-full max-w-6xl flex-col px-4 pt-20 pb-8">
         <EditorClient jobId={jobId} />
       </main>
     </div>
